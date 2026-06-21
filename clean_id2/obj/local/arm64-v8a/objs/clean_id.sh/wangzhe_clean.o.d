67d429273f29db53dcb1b6407113130f7bb6ba66
abcdefgjh-li <3986612313@qq.com>
2026-06-21 22:10:23 +0800

    删除测试文件

--- clean_id2/obj/local/arm64-v8a/objs/clean_id.sh/wangzhe_clean.o.d
+++
@@ -1,6 +0,0 @@
-D:/cpp/obfuscator-ollvm-21.x/clean_id2/obj/local/arm64-v8a/objs/clean_id.sh/wangzhe_clean.o: \
-  D:\cpp\obfuscator-ollvm-21.x\clean_id2\jni\wangzhe_clean.cpp \
-  D:\cpp\obfuscator-ollvm-21.x\clean_id2\jni\weiyan\Util.h \
-  D:\cpp\obfuscator-ollvm-21.x\clean_id2\jni\weiyan\json.hpp
-D:\cpp\obfuscator-ollvm-21.x\clean_id2\jni\weiyan\Util.h:
-D:\cpp\obfuscator-ollvm-21.x\clean_id2\jni\weiyan\json.hpp:
